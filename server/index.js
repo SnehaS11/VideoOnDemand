85de5f107a6e1c3451f50fd7e01fb20a6cb59937
snehass11 <sneha@logisian.in>
2021-03-14 19:25:14 +0530

    Make changes for deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+const path = require('path');
 
 //Connect to database
 connectDB();
@@ -9,14 +10,21 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send("API Running"))
-
 //Define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/videos', require('./routes/api/videos'))
 app.use('/api/speakers', require('./routes/api/speakers'))
 app.use('/api/tags', require('./routes/api/tags'))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
